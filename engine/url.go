69097f84641b8429a60ffe21bc132908fdbae643
Tri Minh Cao <tri@alkira.net>
2019-12-17 18:16:03 -0800

    created a simple test file for url.go

--- engine/url.go
+++ engine/url.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// ShiftPath shifts path one at a time to expose the current level in the path
 func ShiftPath(p string) (head, tail string) {
 	p = path.Clean("/" + p)
 	i := strings.Index(p[1:], "/") + 1
