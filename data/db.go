98b41830c59d102fab6cd7cc479c5aa9c0f0e556
Tri M. Cao <trimcao@gmail.com>
2020-01-14 16:27:00 -0800

    add integration test for mongodb

--- data/db.go
+++ data/db.go
@@ -2,8 +2,19 @@ package data
 
 import "github.com/trimcao/go-saas/data/model"
 
+type SessionRefresher interface {
+	RefreshSession(*model.Connection, string)
+}
+
+type UserServices interface {
+	SessionRefresher
+	GetDetail(id model.Key) (*model.User, error)
+}
+
 type DB struct {
 	DatabaseName string
 	Connection   *model.Connection
 	CopySession  bool
+
+	Users UserServices
 }
